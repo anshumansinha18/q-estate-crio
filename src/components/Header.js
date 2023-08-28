0d4ab81cd55991973dbfd60c810b0b9b272f14e6
anshumansinha1 <anshuman.sinha@criodo.com>
2023-08-28 21:59:47 +0530

    C1 Header Component Added

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="logo">Q-Estate</div>
+      <div className="nav-link">Explore Listings</div>
+    </div>
+  );
+}
---
+++ src/components/LandingPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./Header";
+import "./LandingPage.css";
+
+export default function LandingPage() {
+  return (
+    <div className="landing-page-container">
+      <Header />
+    </div>
+  );
+}
